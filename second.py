ae8c4a00885422425a6c7199f2bb7170201b8245
zharasbayevatomiris <zharas.tomi@gmail.com>
2024-10-17 15:55:45 +0200

    Update second.py

--- second.py
+++ second.py
@@ -1,12 +1,4 @@
-def cislo_text(cislo):
-    # funkce zkonvertuje cislo do jeho textove reprezentace
-    # napr: "25" -> "dvacet pět", omezte se na cisla od 0 do 100
-    return "dvacet pět"
 
-if __name__ == "__main__":
-    cislo = input("Zadej číslo: ")
-    text = cislo_text(cislo)
-    print(text)
 #ukol 2
 def cislo_text(cislo):
     cisla = {
