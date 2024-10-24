1101bb29816ec58124e6d9c67489ff2f600aeead
zharasbayevatomiris <zharas.tomi@gmail.com>
2024-10-24 08:58:25 +0000

    something

--- pokus4(2).py
+++ pokus4(2).py
@@ -1,5 +1,29 @@
 def levensteinova_vzdalenost(dotaz1, dotaz2):
-    pass
+   """
+   Levensteinova vzdalenost 
+   """
+   lenght = max(len(dotaz1), len(dotaz2))
+   i = 0
+   levenstein = 0 
+   while i < lenght: 
+     if i < len(dotaz1) and i < len(dotaz2)
+         if dotaz1[i] != dotaz2[i]:
+            levenstein += 1
+    else:
+        levenstein += 1
+    i += 1
+    return levenstein
+
+    def levensteinova_vzdalenost2(dotaz1, dotaz2):
+        levenstein = 0
+        lenght = min(len(dotaz1), len(dotaz2))
+        for i in range(lenght):
+            if dotaz1[i] != dotaz2[i]:
+                levenstein += 1
+        levenstein += abs(len(dotaz1) - len(dotaz2))
+        return levenstein
+    
+        
 
 
 if __name__ == "__main__":
@@ -11,4 +35,3 @@ if __name__ == "__main__":
     print(levensteinova_vzdalenost(query1, query2))
     print(levensteinova_vzdalenost(query2, query3))
     print(levensteinova_vzdalenost(query1, query3))
-    
