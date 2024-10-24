5b043587e27e4cc798f9c6e4799bc6d0c75d4a1b
zharasbayevatomiris <zharas.tomi@gmail.com>
2024-10-24 08:20:09 +0000

    something

---
+++ pokus4.py
@@ -0,0 +1,22 @@
+def jaccardova_vzdalenost_mnozin(mnozina1, mnozina2):
+    mnozina1 = set(mnozina1)
+    mnozina2 = set(mnozina2)
+    intersaction 
+
+if __name__ == "__main__":
+    serp1 = ["https://www.seznam.cz", "https://www.google.com", "https://www.jcu.cz", "https://www.czu.cz", "https://www.cvut.cz", "https://www.uk.cz"]
+    serp2 = ["https://www.seznam.cz", "https://www.google.com", "https://www.novinky.cz", "https://www.idnes.cz", "https://www.zpravy.cz", "https://www.tn.cz"]
+    serp3 = ["https://www.jcu.cz", "https://www.czu.cz", "https://www.cvut.cz", "https://www.uk.cz"]
+
+    print(jaccardova_vzdalenost_mnozin(serp1, serp2))
+    print(jaccardova_vzdalenost_mnozin(serp2, serp3))
+    print(jaccardova_vzdalenost_mnozin(serp1, serp3))
+
+    set_
+
+
+
+
+    #2 uloha
+
+    #"seznam"  "seznamka"
