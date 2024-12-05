14cdb2b3c6730f0b438fc48a47611423fe129b13
zharasbayevatomiris <zharas.tomi@gmail.com>
2024-12-05 09:59:50 +0000

    smth

---
+++ cviceni9.py
@@ -0,0 +1,9 @@
+def sudy_lichy(cislo):
+    return None 
+
+
+    def test_sudy_lichy():
+        assert sudy_lichy(1) == "lichy"
+        assert sudy_lichy(2) == "sudy"
+        assert sudy_lichy(100) == "sudy"
+        assert sudy_lichy(10001) == 
